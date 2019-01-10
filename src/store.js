645ef262d543f459d558f2aaa076c0240521d746
ENvironmentSet <sbshw1@gmail.com>
2019-01-10 15:29:08 +0900

    refactor store
    boilerplate is replaced by enhance

--- src/store.js
+++ src/store.js
@@ -1,26 +1,6 @@
-const INIT = {
-  type: '@@INIT',
-};
+import { enhance } from '^';
 
-export const store = (reducer, preloadedState = reducer(undefined, INIT)) => {
-  let state = preloadedState;
-  let subscribers = [];
-
-  return (start, sink) => {
-    if (start !== 0) return;
-    const subscriber = () => sink(1, state);
-    const subscriberIndex = subscribers.push(subscriber) - 1;
-
-    sink(0, (t, d) => {
-      if (t === 1) {
-        state = reducer(state, d);
-        subscribers.forEach(subscriber => subscriber());
-      } else if (t === 2) {
-        subscribers.splice(subscriberIndex, 1);
-      }
-    });
-  };
-};
+export const store = enhance();
 
 export const dispatch = store => action$ => {
   store(0, (t, d) => {
